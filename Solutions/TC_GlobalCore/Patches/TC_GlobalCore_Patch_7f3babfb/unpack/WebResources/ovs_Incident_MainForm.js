2ef863c86ff258380c7b66616381c2b6a256b484
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2024-11-25 19:07:01 +0000

    Update Solution - TC_GlobalCore_Patch_7f3babfb:

---
+++ Solutions/TC_GlobalCore/Patches/TC_GlobalCore_Patch_7f3babfb/unpack/WebResources/ovs_Incident_MainForm.js
@@ -0,0 +1,108 @@
+﻿///<reference path="../../Utilities/GlobalHelper.js"/>
+///<reference path="../../Utilities/questionnaireFunctions.js"/>
+
+
+var TDG_Incident_MainForm = (function (window, document) {
+    //variables
+    var formType;
+    var facilityTypeExcluded = new Array(1, 12, 15, 19, 22);
+
+
+    //********************private methods*******************
+
+
+    function filterFacilityType(formContext) {
+
+        glHelper.filterOptionSet(formContext, "ovs_facility_type_cd", facilityTypeExcluded, false);
+    }
+
+    function seriousJeopardyBehavior(formContext) {
+
+        Xrm.WebApi.online.retrieveMultipleRecords("ovs_incident_report", "?$select=ovs_incident_reportid,ovs_mode_cd,ovs_primary_report_ind&$filter=ovs_mode_cd eq 4 and  _ovs_incident_id_value eq " + formContext.data.entity.getId().replace('{', '').replace('}', '') + " and  ovs_primary_report_ind eq true").then(
+            function success(results) {
+                glHelper.SetDisabled(formContext, "ovs_serious_jeopardy_cd", !(results.entities.length > 0));
+            },
+            function (error) {
+                glHelper.SetDisabled(formContext, "ovs_serious_jeopardy_cd", true);
+                Xrm.Navigation.openErrorDialog({ message: "Something went wrong checking Seriouse Jeopardy. Error: " + error.message });
+            }
+        );
+    }
+
+    //********************private methods end***************
+
+    //********************public methods***************
+    return {
+
+        OnLoad: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            formType = glHelper.GetFormType(formContext);
+
+            filterFacilityType(formContext);
+
+            var phImpact = formContext.getAttribute("ovs_physical_impact_cds");
+            phImpact.removeOnChange(TDG_Incident_MainForm.PhisicalImpact_OnChange);
+            phImpact.addOnChange(TDG_Incident_MainForm.PhisicalImpact_OnChange);
+           
+            var phImpactFlag = formContext.getAttribute("ovs_physical_impact_flag_ind");
+            phImpactFlag.removeOnChange(TDG_Incident_MainForm.PhisicalImpactFlag_OnChange);
+            phImpactFlag.addOnChange(TDG_Incident_MainForm.PhisicalImpactFlag_OnChange);
+            phImpactFlag.fireOnChange();
+
+            var contributingFactors = formContext.getAttribute("ovs_contributing_factors_cds");
+            contributingFactors.removeOnChange(TDG_Incident_MainForm.ContributingFactors_OnChange);
+            contributingFactors.addOnChange(TDG_Incident_MainForm.ContributingFactors_OnChange);
+
+            ////initiate Copy Address control
+            //var wrControl = formContext.getControl("WebResource_CopyButton");
+            //if (wrControl) wrControl.getContentWindow().then(function (contentWindow) {contentWindow.setClientApiContext(Xrm, formContext);})  
+
+            if (formType == glHelper.FORMTYPE_CREATE) {
+
+
+            }
+            else {
+
+                seriousJeopardyBehavior(formContext);
+
+                var exceptionExemtion = formContext.getAttribute("ovs_exemption_ind");
+                exceptionExemtion.removeOnChange(TDG_Incident_MainForm.Exemtion_OnChange);
+                exceptionExemtion.addOnChange(TDG_Incident_MainForm.Exemtion_OnChange);
+            }
+        },
+
+        PhisicalImpact_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            glHelper.openOtherFromChoice_s(formContext, "ovs_physical_impact_cds", "9", "ovs_other_physical_impact_txt");
+        },
+
+        PhisicalImpactFlag_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            var flagOn = formContext.getAttribute("ovs_physical_impact_flag_ind").getValue();
+            if (!flagOn) glHelper.SetValue(formContext, "ovs_physical_impact_cds", null);
+            glHelper.SetControlReadOnly(formContext, "ovs_physical_impact_cds", !flagOn);
+            //glHelper.SetControlVisibility(formContext, "ovs_physical_impact_cds", flagOn);
+            TDG_Incident_MainForm.PhisicalImpact_OnChange(executionContext);
+        },
+
+        ContributingFactors_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            glHelper.openOtherFromChoice_s(formContext, "ovs_contributing_factors_cds", "9", "ovs_contributing_factor_other_txt");
+        },
+
+        Exemtion_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            glHelper.DisplayFormNotificationModern(formContext, "Change of 'Exception/Exemption' may affect 'Reportable' status  calculation. Please, recalculate the scope.", "WARNING", true, 50000);
+        },
+
+    };
+    //********************public methods end***************
+})(window, document);
---
+++ Solutions/TC_GlobalCore/Patches/TC_GlobalCore_Patch_7f3babfb/unpack/WebResources/ovs_incident_report_main.js
@@ -0,0 +1,147 @@
+﻿///<reference path="../../Utilities/GlobalHelper.js"/>
+///<reference path="../../Utilities/questionnaireFunctions.js"/>
+
+
+var Incident_Report= (function (window, document) {
+    //variables
+    var formType;
+    var facilityTypeExcluded = new Array(1, 12, 15, 19, 22);
+
+    var PostalAdressArray = new Array("ovs_street1_txt", "ovs_street2_txt", "ovs_street3_txt", "ovs_city_txt", "ovs_state_province_txt", "ovs_zip_postalcode_txt", "ovs_country_txt");
+    var LongLatArray = new Array("ovs_latitude_nbr", "ovs_longitude_nbr");
+    var LLD_Array = new Array("ovs_quarter_lsd_cd", "ovs_range_cd", "ovs_section_cd", "ovs_township_cd");
+
+
+    //********************private methods*******************
+
+    function clearHideEase(formContext, arrayOfFields, doClearHideEase) {
+
+        for (var i = 0; i < arrayOfFields.length; i++) {
+            if (doClearHideEase) glHelper.SetValue(formContext, arrayOfFields[i], null);
+            glHelper.SetControlVisibility(formContext, arrayOfFields[i], !doClearHideEase);
+            glHelper.SetRequiredLevel(formContext, arrayOfFields[i], !doClearHideEase);
+        }
+    }
+
+    //********************private methods end***************
+
+    //********************public methods***************
+    return {
+
+        OnLoad: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            formType = glHelper.GetFormType(formContext);
+
+            var source = formContext.getAttribute("ovs_source_cd");
+            source.removeOnChange(Incident_Report.Source_OnChange);
+            source.addOnChange(Incident_Report.Source_OnChange);
+            source.fireOnChange();
+
+            var releasetype = formContext.getAttribute("ovs_release_type_cds");
+            releasetype.removeOnChange(Incident_Report.ReleaseType_OnChange);
+            releasetype.addOnChange(Incident_Report.ReleaseType_OnChange);
+            releasetype.fireOnChange();
+
+            var closure = formContext.getAttribute("ovs_closure_ind");
+            closure.removeOnChange(Incident_Report.Closure_OnChange);
+            closure.addOnChange(Incident_Report.Closure_OnChange);
+            closure.fireOnChange();
+
+            var evacuatioFlag = formContext.getAttribute("ovs_evacuation_due_incident_ind");
+            evacuatioFlag.removeOnChange(Incident_Report.EvacuationProfileFlag_OnChange);
+            evacuatioFlag.addOnChange(Incident_Report.EvacuationProfileFlag_OnChange);
+            evacuatioFlag.fireOnChange();
+
+            var emergencyPers = formContext.getAttribute("ovs_emergency_response_personnel_cds");
+            emergencyPers.removeOnChange(Incident_Report.EmergencyPersonnel_OnChange);
+            emergencyPers.addOnChange(Incident_Report.EmergencyPersonnel_OnChange);
+
+            ////initiate Copy Address control
+            //var wrControl = formContext.getControl("WebResource_CopyButton");
+            //if (wrControl) wrControl.getContentWindow().then(function (contentWindow) {contentWindow.setClientApiContext(Xrm, formContext);})  
+
+            if (formType == glHelper.FORMTYPE_CREATE) {
+
+
+            }
+            else {
+                emergencyPers.fireOnChange();
+            }
+        },
+
+        Source_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            glHelper.openOtherFromChoice(formContext, "ovs_source_cd", "23", "ovs_other_source_txt");
+            glHelper.openOtherFromChoice(formContext, "ovs_source_cd", "1", "ovs_initial_30day_follow_up_receipt_date_dte");
+       },
+
+        ReleaseType_OnChange: async function (executionContext) {
+            try {
+                var formContext = executionContext.getFormContext();
+                var multiSelectOptionSet = formContext.getAttribute("ovs_release_type_cds").getValue();
+                if (multiSelectOptionSet != null) {
+                    let optionString = multiSelectOptionSet.toString();
+                    if (optionString.indexOf("918640008") !== -1) {
+                        formContext.getControl("ovs_release_type_other_txt").setVisible(true);
+                        formContext.getAttribute("ovs_release_type_other_txt").setRequiredLevel("required");
+                    } else {
+                        formContext.getControl("ovs_release_type_other_txt").setVisible(false);
+                        formContext.getAttribute("ovs_release_type_other_txt").setRequiredLevel("none");
+                    }
+                    if (optionString.indexOf("918640004") !== -1) {
+                        formContext.getAttribute("ovs_release_type_cds").setValue([918640004])
+                        formContext.getControl("ovs_release_type_other_txt").setVisible(false);
+                        formContext.getAttribute("ovs_release_type_other_txt").setRequiredLevel("none");
+                    }
+               } else {
+                    formContext.getControl("ovs_release_type_other_txt").setVisible(false);
+                    formContext.getAttribute("ovs_release_type_other_txt").setRequiredLevel("none");
+                }
+            } catch (error) {
+                Xrm.Navigation.openErrorDialog({ message: error })
+
+            } finally {
+            }
+        },
+
+        Closure_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+   
+            var isClosure = formContext.getAttribute("ovs_closure_ind").getValue() ;
+
+            if (!isClosure) {
+                formContext.getAttribute("ovs_closure_type_cds").setValue(null);
+            }
+            glHelper.SetDisabled(formContext, "ovs_closure_type_cds", !isClosure);
+            glHelper.SetDisabled(formContext, "ovs_duration_closure_num", !isClosure);
+
+            glHelper.SetRequiredLevel(formContext, "ovs_closure_type_cds", isClosure);
+        },
+
+        EvacuationProfileFlag_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            var evacuationFields = new Array("ovs_evacuation_profile_cds", "ovs_evacuation_count_num", "ovs_duration_evacuation_nbr");
+            var flagOn = formContext.getAttribute("ovs_evacuation_due_incident_ind").getValue();
+
+            for (var i = 0; i < evacuationFields.length; i++) {
+
+                if (!flagOn) glHelper.SetValue(formContext, evacuationFields[i], null);
+                glHelper.SetControlReadOnly(formContext, evacuationFields[i], !flagOn);
+            }
+        },
+
+        EmergencyPersonnel_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            glHelper.openOtherFromChoice_s(formContext, "ovs_emergency_response_personnel_cds", "21", "ovs_other_emergency_response_personnel_txt");
+        },
+
+    };
+    //********************public methods end***************
+})(window, document);
